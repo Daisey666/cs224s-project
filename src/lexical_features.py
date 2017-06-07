88e85f2d08e904281ab308b56a96be7acfcda564
Amanda Yuling Chow <aychow256@gmail.com>
2017-06-07 03:07:40 -0700

    dividing clips into timesteps for rnn

--- src/lexical_features.py
+++ src/lexical_features.py
@@ -1,7 +1,7 @@
 import pdb
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-WORD_VEC_PATH = "afs/ir/users/j/w/jwlouie/cs224s/final-project/cs224s-project/glove/glove.6B.300d.txt"
+WORD_VEC_PATH = "afs/ir/users/j/w/jwlouie/cs224s/final-project/cs224s-project/glove/glove.6B.100d.txt"
 SENTENCES_FILENAME = "/afs/ir/users/j/w/jwlouie/cs224s/final-project/cs224s-project/raw_data/sentences.txt"
 OUTPUT_FILENAME = "/afs/ir/users/j/w/jwlouie/cs224s/final-project/cs224s-project/raw_data/qa_glove_features.txt"
 
